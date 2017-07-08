4e5655cc24239c3f2f031a7aa22ed58ee3bc0836
ttxxly <ttxxly@gmail.com>
2017-07-08 23:22:56 +0800

    修改 注册接口名为 register.php。
    Signed-off-by: ttxxly <ttxxly@gmail.com>

---
+++ back-end/register.php
@@ -0,0 +1,58 @@
+﻿<?php
+/**
+ * Created by PhpStorm.
+ * User: 鑫
+ * Date: 2017/6/27
+ * Time: 14:57
+ */
+
+//链接数据库
+$conn = mysql_connect("localhost", "root", "") or die("数据库连接错误" . mysql_error());
+mysql_select_db("photograph", $conn) or die("数据库访问错误" . mysql_error());
+mysql_query("SET NAMES UTF8");
+
+//注册
+$nickname = @$_GET['nickname'];
+$password = @$_GET['password'];
+$mobile = @$_GET['mobile'];
+
+$sqla = "select * from user where nickname='$nickname'";
+$sqlb = "select * from user where mobile='$mobile'";
+$result1 = mysql_query($sqla);
+$row1 = mysql_fetch_row($result1);
+$result2 = mysql_query($sqlb);
+$row2 = mysql_fetch_row($result2);
+if (!$row1 && !$row2) {
+    $sql = "INSERT INTO `user` (`nickname`, `password`, `mobile`, `portrait`) VALUES('$nickname','$password','$mobile','/photos/portrait/1.jpg')";
+    $result = mysql_query($sql);
+    $id = mysql_insert_id($conn);
+    if ($result) {
+        $arr = array(
+            'flat' => 'success',
+            'message' => '注册成功',
+            'userid' => $id,
+            'nickname' => $nickname,
+            'password' => $password,
+            'mobile' => $mobile,
+            'portrait' => '/photos/portrait/1.jpg'
+        );
+    } else {
+        $arr = array(
+            'flat' => 'fail',
+            'message' => '数据库错误'
+        );
+    }
+} else {
+    $arr = array(
+        'flat' => 'fail',
+        'message' => '手机号或者账户名已存在'
+    );
+}
+
+foreach ($arr as $key => $value) {
+    $arr[$key] = urlencode($value);
+}
+//返给客户端 JSON 数据
+echo urldecode(json_encode($arr));
+
+?>
